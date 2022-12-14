8ca3aed50be3be34df49c63cbb3c53b72392853b
mamkinShcoder <Alexcoolarbitr@gmail.com>
2022-12-14 17:06:56 +0300

    Решена задача "Функция, удваивающая значение переменной".

---
+++ src/Two_Challenge.java
@@ -0,0 +1,13 @@
+// Создать функцию, удваивающую число.
+public class Two_Challenge {
+    public static void main(String[] args) {
+        int i=4;
+         i = Multiply2(i);
+        System.out.println(i);
+    }
+
+    public static int Multiply2 (int i){
+        i = i*2;
+        return i;
+    }
+}
