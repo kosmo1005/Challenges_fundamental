24e93b97ae487a81e1bd7056dcdcacf1008c8d8a
mamkinShcoder <Alexcoolarbitr@gmail.com>
2022-12-16 23:45:23 +0300

    Решена 7 задача "Считаем овец".

---
+++ src/Seven_Challenge.java
@@ -0,0 +1,15 @@
+// Исходя из заданного числа выдать строку считалочку. (3) (1 овца... 2 овца... 3 овца).
+public class Seven_Challenge {
+    public static void main(String[] args) {
+        for (int i=0;i<4;i++) {
+            int a = (int) (Math.random() * 15);
+            System.out.print("(" + a + ")");
+            StringBuffer g = new StringBuffer("");
+            int w=1;
+            for (int o=0;o<a;o++){
+                g.append((w++) + " овца... ");
+            }
+            System.out.println(g);
+        }
+    }
+}
